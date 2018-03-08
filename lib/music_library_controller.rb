7ece896158dfb8934699fee4a21c6cbab09a2b44
Laura <laurao148@gmail.com>
2018-03-08 20:03:27 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,34 +7,37 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
 
-    input = gets.strip
+    while input != "exit" do
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
-    if input == exit
-      return
-    elsif input == "list songs"
-      self.list_songs
-    elsif input == "list artists"
-      self.list_artists
-    elsif input == "list genres"
-      self.list_genres
-    elsif input == "list artist"
-      self.list_songs_by_artist
-    elsif input == "list genre"
-      self.list_songs_by_genre
-    elsif input == "play song"
-      self.play_song
-    else
-      self.call
+      input = gets.strip
+
+      if input == exit
+        return
+      elsif input == "list songs"
+        self.list_songs
+      elsif input == "list artists"
+        self.list_artists
+      elsif input == "list genres"
+        self.list_genres
+      elsif input == "list artist"
+        self.list_songs_by_artist
+      elsif input == "list genre"
+        self.list_songs_by_genre
+      elsif input == "play song"
+        self.play_song
+      else
+        self.call
+      end
     end
   end
 
